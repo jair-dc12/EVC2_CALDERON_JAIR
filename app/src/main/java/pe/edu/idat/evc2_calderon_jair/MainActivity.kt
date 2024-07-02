6291d5bf8efdd21968669a82766cdc1dead1ce21
Jair <jaircald2020@gmail.com>
2024-07-01 21:17:50 -0500

    ejercicio 1_1

---
+++ app/src/main/java/pe/edu/idat/evc2_calderon_jair/MainActivity.kt
@@ -0,0 +1,47 @@
+package pe.edu.idat.evc2_calderon_jair
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import pe.edu.idat.evc2_calderon_jair.ui.theme.EVC2_CALDERON_JAIRTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            EVC2_CALDERON_JAIRTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    EVC2_CALDERON_JAIRTheme {
+        Greeting("Android")
+    }
+}
